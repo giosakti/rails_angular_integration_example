79d824fb285c9738d6e6c075a28b94d241af06d4
Giovanni Sakti <giosakti@starqle.com>
2013-04-08 14:59:50 +0700

    include jquery for csrf.js.coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
 //= require angular
 //= require angular-ui-states
 //= require app/main
